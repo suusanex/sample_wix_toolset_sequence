b05246ee46a76e6de41563f78fccb43c394d59cd
suusanex <suusan@himado.net>
2023-03-14 09:46:56 +0900

    インストール先ファイルの各カスタムアクションでの状態を動作確認するログを追加

--- sample_wix_toolset_sequence/sample_wix_toolset_sequence_ca/CustomAction.cs
+++ sample_wix_toolset_sequence/sample_wix_toolset_sequence_ca/CustomAction.cs
@@ -1,16 +1,32 @@
 using Microsoft.Deployment.WindowsInstaller;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace sample_wix_toolset_sequence_ca
 {
     public class CustomActions
     {
+        private static readonly string InstallFilePath =
+            Environment.ExpandEnvironmentVariables(@"%ProgramFiles%\sample_wix_toolset_sequence\README.md");
+
+        static string InstallFileTrace()
+        {
+            if (File.Exists(InstallFilePath))
+            {
+                return $"InstallFilePath Exist, Data={File.ReadAllText(InstallFilePath)}";
+            }
+            else
+            {
+                return "InstallFilePath Not Exist";
+            }
+        }
+
         [CustomAction]
         public static ActionResult InstallAndUpgradeFilesCustomAction(Session session)
         {
-            session.Log($"Begin InstallAndUpgradeFilesCustomAction, TempDir={session.CustomActionData["TempDir"]}, ProductVersion={session.CustomActionData["ProductVersion"]}, SourceDir={session.CustomActionData["SourceDir"]}");
+            session.Log($"Begin InstallAndUpgradeFilesCustomAction, TempDir={session.CustomActionData["TempDir"]}, ProductVersion={session.CustomActionData["ProductVersion"]}, SourceDir={session.CustomActionData["SourceDir"]}, InstallFile={InstallFileTrace()}");
 
             return ActionResult.Success;
         }
@@ -18,7 +34,7 @@ namespace sample_wix_toolset_sequence_ca
         [CustomAction]
         public static ActionResult InstallAndUpgradeFilesCustomActionRollback(Session session)
         {
-            session.Log($"Begin InstallAndUpgradeFilesCustomActionRollback, ProductVersion={session.CustomActionData["ProductVersion"]}");
+            session.Log($"Begin InstallAndUpgradeFilesCustomActionRollback, ProductVersion={session.CustomActionData["ProductVersion"]}, InstallFile={InstallFileTrace()}");
 
             return ActionResult.Success;
         }
@@ -26,7 +42,7 @@ namespace sample_wix_toolset_sequence_ca
         [CustomAction]
         public static ActionResult InstallOnlyFilesCustomAction(Session session)
         {
-            session.Log($"Begin InstallOnlyFilesCustomAction, ProductVersion={session.CustomActionData["ProductVersion"]}");
+            session.Log($"Begin InstallOnlyFilesCustomAction, ProductVersion={session.CustomActionData["ProductVersion"]}, InstallFile={InstallFileTrace()}");
 
             return ActionResult.Success;
         }
@@ -34,7 +50,7 @@ namespace sample_wix_toolset_sequence_ca
         [CustomAction]
         public static ActionResult InstallOnlyFilesCustomActionRollback(Session session)
         {
-            session.Log($"Begin InstallOnlyFilesCustomActionRollback, ProductVersion={session.CustomActionData["ProductVersion"]}");
+            session.Log($"Begin InstallOnlyFilesCustomActionRollback, ProductVersion={session.CustomActionData["ProductVersion"]}, InstallFile={InstallFileTrace()}");
 
             return ActionResult.Success;
         }
@@ -42,7 +58,7 @@ namespace sample_wix_toolset_sequence_ca
         [CustomAction]
         public static ActionResult UninstallFilesCustomAction(Session session)
         {
-            session.Log($"Begin UninstallFilesCustomAction, ProductVersion={session.CustomActionData["ProductVersion"]}");
+            session.Log($"Begin UninstallFilesCustomAction, ProductVersion={session.CustomActionData["ProductVersion"]}, InstallFile={InstallFileTrace()}");
 
             return ActionResult.Success;
         }
@@ -50,7 +66,7 @@ namespace sample_wix_toolset_sequence_ca
         [CustomAction]
         public static ActionResult UninstallFilesCustomActionRollback(Session session)
         {
-            session.Log($"Begin UninstallFilesCustomActionRollback, ProductVersion={session.CustomActionData["ProductVersion"]}");
+            session.Log($"Begin UninstallFilesCustomActionRollback, ProductVersion={session.CustomActionData["ProductVersion"]}, InstallFile={InstallFileTrace()}");
 
             return ActionResult.Success;
         }
@@ -58,7 +74,7 @@ namespace sample_wix_toolset_sequence_ca
         [CustomAction]
         public static ActionResult UpgradingUninstallFilesCustomAction(Session session)
         {
-            session.Log($"Begin UpgradingUninstallFilesCustomAction, ProductVersion={session.CustomActionData["ProductVersion"]}");
+            session.Log($"Begin UpgradingUninstallFilesCustomAction, ProductVersion={session.CustomActionData["ProductVersion"]}, InstallFile={InstallFileTrace()}");
 
             return ActionResult.Success;
         }
@@ -66,7 +82,7 @@ namespace sample_wix_toolset_sequence_ca
         [CustomAction]
         public static ActionResult UpgradingUninstallFilesCustomActionRollback(Session session)
         {
-            session.Log($"Begin UpgradingUninstallFilesCustomActionRollback, ProductVersion={session.CustomActionData["ProductVersion"]}");
+            session.Log($"Begin UpgradingUninstallFilesCustomActionRollback, ProductVersion={session.CustomActionData["ProductVersion"]}, InstallFile={InstallFileTrace()}");
 
             return ActionResult.Success;
         }
@@ -74,7 +90,7 @@ namespace sample_wix_toolset_sequence_ca
         [CustomAction]
         public static ActionResult UpgradeInstallFilesCustomAction(Session session)
         {
-            session.Log($"Begin UpgradeInstallFilesCustomAction, ProductVersion={session.CustomActionData["ProductVersion"]}");
+            session.Log($"Begin UpgradeInstallFilesCustomAction, ProductVersion={session.CustomActionData["ProductVersion"]}, InstallFile={InstallFileTrace()}");
 
             return ActionResult.Success;
         }
@@ -82,7 +98,7 @@ namespace sample_wix_toolset_sequence_ca
         [CustomAction]
         public static ActionResult UpgradeInstallFilesCustomActionRollback(Session session)
         {
-            session.Log($"Begin UpgradeInstallFilesCustomActionRollback, ProductVersion={session.CustomActionData["ProductVersion"]}");
+            session.Log($"Begin UpgradeInstallFilesCustomActionRollback, ProductVersion={session.CustomActionData["ProductVersion"]}, InstallFile={InstallFileTrace()}");
 
             return ActionResult.Success;
         }
