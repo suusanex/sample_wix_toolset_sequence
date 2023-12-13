2df0a1be0448a354e7e0cf201b2e09edc0de2ba3
suusanex <suusan@himado.net>
2023-12-13 14:52:26 +0900

    カスタムアクションを1ペア（実行とロールバック）に統一し、実行条件をプロパティで渡す方式へ変更。

--- sample_wix_toolset_sequence/sample_wix_toolset_sequence_ca/CustomAction.cs
+++ sample_wix_toolset_sequence/sample_wix_toolset_sequence_ca/CustomAction.cs
@@ -24,83 +24,32 @@ namespace sample_wix_toolset_sequence_ca
         }
 
         [CustomAction]
-        public static ActionResult InstallAndUpgradeFilesCustomAction(Session session)
+        public static ActionResult InstallOrUpgradeOrUninstall(Session session)
         {
-            session.Log($"Begin InstallAndUpgradeFilesCustomAction, TempDir={session.CustomActionData["TempDir"]}, ProductVersion={session.CustomActionData["ProductVersion"]}, SourceDir={session.CustomActionData["SourceDir"]}, InstallFile={InstallFileTrace()}");
+            var installTypeParams = string.Join(",",
+                $"InstallTypeUpgrade={session.CustomActionData["InstallTypeUpgrade"]}",
+                $"InstallTypeInstall={session.CustomActionData["InstallTypeInstall"]}",
+                $"InstallTypeUnInstallComplete={session.CustomActionData["InstallTypeUnInstallComplete"]}",
+                $"InstallTypeUninstallForUpgrade={session.CustomActionData["InstallTypeUninstallForUpgrade"]}"
+            );
+            session.Log($"Begin InstallOrUpgradeOrUninstall, installTypeParams={installTypeParams}, TempDir={session.CustomActionData["TempDir"]}, ProductVersion={session.CustomActionData["ProductVersion"]}, SourceDir={session.CustomActionData["SourceDir"]}, InstallFile={InstallFileTrace()}");
 
             return ActionResult.Success;
         }
 
         [CustomAction]
-        public static ActionResult InstallAndUpgradeFilesCustomActionRollback(Session session)
+        public static ActionResult InstallOrUpgradeOrUninstallRollback(Session session)
         {
-            session.Log($"Begin InstallAndUpgradeFilesCustomActionRollback, ProductVersion={session.CustomActionData["ProductVersion"]}, InstallFile={InstallFileTrace()}");
+            var installTypeParams = string.Join(",",
+                $"InstallTypeUpgrade={session.CustomActionData["InstallTypeUpgrade"]}",
+                $"InstallTypeInstall={session.CustomActionData["InstallTypeInstall"]}",
+                $"InstallTypeUnInstallComplete={session.CustomActionData["InstallTypeUnInstallComplete"]}",
+                $"InstallTypeUninstallForUpgrade={session.CustomActionData["InstallTypeUninstallForUpgrade"]}"
+            );
+            session.Log($"Begin InstallOrUpgradeOrUninstallRollback, installTypeParams={installTypeParams}, ProductVersion={session.CustomActionData["ProductVersion"]}, InstallFile={InstallFileTrace()}");
 
             return ActionResult.Success;
         }
 
-        [CustomAction]
-        public static ActionResult InstallOnlyFilesCustomAction(Session session)
-        {
-            session.Log($"Begin InstallOnlyFilesCustomAction, ProductVersion={session.CustomActionData["ProductVersion"]}, InstallFile={InstallFileTrace()}");
-
-            return ActionResult.Success;
-        }
-
-        [CustomAction]
-        public static ActionResult InstallOnlyFilesCustomActionRollback(Session session)
-        {
-            session.Log($"Begin InstallOnlyFilesCustomActionRollback, ProductVersion={session.CustomActionData["ProductVersion"]}, InstallFile={InstallFileTrace()}");
-
-            return ActionResult.Success;
-        }
-
-        [CustomAction]
-        public static ActionResult UninstallFilesCustomAction(Session session)
-        {
-            session.Log($"Begin UninstallFilesCustomAction, ProductVersion={session.CustomActionData["ProductVersion"]}, InstallFile={InstallFileTrace()}");
-
-            return ActionResult.Success;
-        }
-
-        [CustomAction]
-        public static ActionResult UninstallFilesCustomActionRollback(Session session)
-        {
-            session.Log($"Begin UninstallFilesCustomActionRollback, ProductVersion={session.CustomActionData["ProductVersion"]}, InstallFile={InstallFileTrace()}");
-
-            return ActionResult.Success;
-        }
-
-        [CustomAction]
-        public static ActionResult UpgradingUninstallFilesCustomAction(Session session)
-        {
-            session.Log($"Begin UpgradingUninstallFilesCustomAction, ProductVersion={session.CustomActionData["ProductVersion"]}, InstallFile={InstallFileTrace()}");
-
-            return ActionResult.Success;
-        }
-
-        [CustomAction]
-        public static ActionResult UpgradingUninstallFilesCustomActionRollback(Session session)
-        {
-            session.Log($"Begin UpgradingUninstallFilesCustomActionRollback, ProductVersion={session.CustomActionData["ProductVersion"]}, InstallFile={InstallFileTrace()}");
-
-            return ActionResult.Success;
-        }
-
-        [CustomAction]
-        public static ActionResult UpgradeInstallFilesCustomAction(Session session)
-        {
-            session.Log($"Begin UpgradeInstallFilesCustomAction, ProductVersion={session.CustomActionData["ProductVersion"]}, InstallFile={InstallFileTrace()}");
-
-            return ActionResult.Success;
-        }
-
-        [CustomAction]
-        public static ActionResult UpgradeInstallFilesCustomActionRollback(Session session)
-        {
-            session.Log($"Begin UpgradeInstallFilesCustomActionRollback, ProductVersion={session.CustomActionData["ProductVersion"]}, InstallFile={InstallFileTrace()}");
-
-            return ActionResult.Success;
-        }
     }
 }
